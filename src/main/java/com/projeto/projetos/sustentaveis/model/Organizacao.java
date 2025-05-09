d7d86238ab2992371c15174fce27ec17461db056
Andressa Costa <andressacosta@MacBook-Air-de-Andressa.local>
2025-05-09 20:26:30 -0300

    M1S10-Ex-4-CRUD-de-Orgnização

--- src/main/java/com/projeto/projetos/sustentaveis/model/Organizacao.java
+++ src/main/java/com/projeto/projetos/sustentaveis/model/Organizacao.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 @Entity
 public class Organizacao {
 
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/projeto/projetos/sustentaveis/repository/OrganizacaoRepository.java
+++ src/main/java/com/projeto/projetos/sustentaveis/repository/OrganizacaoRepository.java
@@ -3,5 +3,6 @@ package com.projeto.projetos.sustentaveis.repository;
 import com.projeto.projetos.sustentaveis.model.Organizacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface OrganizacaoRepository extends JpaRepository<Organizacao, Long> {
 }
